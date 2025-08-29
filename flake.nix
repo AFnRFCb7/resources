cf1fdfb7bcf4ddab51cfd9ba464631994103da6b
Emory Merryman <emory.merryman@gmail.com>
2025-08-29 08:00:43 -0400

    work on setup .. use the new testing-locks_ lambda

--- flake.nix
+++ flake.nix
@@ -514,14 +514,14 @@
                                                                     if builtins.typeOf init == "null" then
                                                                         ''
                                                                             mkdir --parents "${ resources-directory }/locks"
-                                                                            ${ if builtins.typeOf testing-locks == "bool" && testing-locks then "exec 200> ${ resources-directory }/locks/test.setup.lock" else "#" }
-                                                                            ${ if builtins.typeOf testing-locks == "bool" && testing-locks then "flock -s 200" else "#" }
-                                                                            ${ if builtins.typeOf testing-locks == "bool" && testing-locks then "exec 201> ${ resources-directory }/locks/test.stall-for-process.lock" else "#" }
-                                                                            ${ if builtins.typeOf testing-locks == "bool" && testing-locks then "flock -s 201" else "#" }
-                                                                            ${ if builtins.typeOf testing-locks == "bool" && testing-locks then "exec 202> ${ resources-directory }/locks/test.stall-for-cleanup.lock" else "#" }
-                                                                            ${ if builtins.typeOf testing-locks == "bool" && testing-locks then "flock -s 202" else "#" }
-                                                                            ${ if builtins.typeOf testing-locks == "bool" && testing-locks then "exec 203> ${ resources-directory }/locks/test.teardown.lock" else "#" }
-                                                                            ${ if builtins.typeOf testing-locks == "bool" && testing-locks then "flock -s 203" else "#" }
+                                                                            ${ if testing-locks_ then "exec 200> ${ resources-directory }/locks/test.setup.lock" else "#" }
+                                                                            ${ if testing-locks_ then "flock -s 200" else "#" }
+                                                                            ${ if testing-locks_ then "exec 201> ${ resources-directory }/locks/test.stall-for-process.lock" else "#" }
+                                                                            ${ if testing-locks_ then "flock -s 201" else "#" }
+                                                                            ${ if testing-locks_ then "exec 202> ${ resources-directory }/locks/test.stall-for-cleanup.lock" else "#" }
+                                                                            ${ if testing-locks_ then "flock -s 202" else "#" }
+                                                                            ${ if testing-locks_ then "exec 203> ${ resources-directory }/locks/test.teardown.lock" else "#" }
+                                                                            ${ if testing-locks_ then "flock -s 203" else "#" }
                                                                             if [[ -t 0 ]]
                                                                             then
                                                                                 HAS_STANDARD_INPUT=false
